42c6453b81fc574386ef815726283c7c0a102967
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-03-18 22:42:34 -0400

    Complete assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    [print(f'{names.index(name) + 1}. {name}') for name in names]
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(names):
+    return [name.capitalize() + '!' for name in names]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    if [True for long in calls if len(long) > 4]:
+        return True
+    else:
+        return False    
+
+def find_the_cheese(items):
+    if 'cheddar' in items:
+        return 'cheddar'
+    elif 'gouda' in items:
+        return 'gouda'
+    elif 'camembert' in items:
+        return 'camembert'
